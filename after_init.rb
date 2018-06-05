8598c0392af7ac24209e61179d0144c04c704481
petr <petr@easy.cz>
2018-06-05 09:31:09 +0200

    QQ Connect support

--- after_init.rb
+++ after_init.rb
@@ -8,21 +8,26 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, setup: -> (env) {
     env['omniauth.strategy'].options[:client_id]     = Setting.plugin_redmine_social_sign_in['google_app_id']
     env['omniauth.strategy'].options[:client_secret] = Setting.plugin_redmine_social_sign_in['google_app_secret']
-  }, scope: 'email,profile', path_prefix: '/social_sign_in'
+  }, scope:                       'email,profile', path_prefix: '/social_sign_in'
 
   provider :facebook, setup: -> (env) {
     env['omniauth.strategy'].options[:client_id]     = Setting.plugin_redmine_social_sign_in['facebook_app_id']
     env['omniauth.strategy'].options[:client_secret] = Setting.plugin_redmine_social_sign_in['facebook_app_secret']
-  }, scope: 'email,public_profile', path_prefix: '/social_sign_in'
+  }, scope:                  'email,public_profile', path_prefix: '/social_sign_in'
 
   provider :linkedin, setup: -> (env) {
     env['omniauth.strategy'].options[:client_id]     = Setting.plugin_redmine_social_sign_in['linkedin_app_id']
     env['omniauth.strategy'].options[:client_secret] = Setting.plugin_redmine_social_sign_in['linkedin_app_secret']
-  }, path_prefix: '/social_sign_in'
+  }, path_prefix:            '/social_sign_in'
 
   provider :twitter, setup: -> (env) {
     env['omniauth.strategy'].options[:client_id]     = Setting.plugin_redmine_social_sign_in['twitter_app_id']
     env['omniauth.strategy'].options[:client_secret] = Setting.plugin_redmine_social_sign_in['twitter_app_secret']
-  }, path_prefix: '/social_sign_in'
+  }, path_prefix:           '/social_sign_in'
+
+  provider :qq_connect, setup: -> (env) {
+    env['omniauth.strategy'].options[:client_id]     = Setting.plugin_redmine_social_sign_in['qq_connect_app_id']
+    env['omniauth.strategy'].options[:client_secret] = Setting.plugin_redmine_social_sign_in['qq_connect_app_secret']
+  }, path_prefix:              '/social_sign_in'
 end
 
--- init.rb
+++ init.rb
@@ -3,23 +3,26 @@ Redmine::Plugin.register :redmine_social_sign_in do
   author 'Easy Software Ltd'
   description 'sign in through social network like Google, Facebook and others '
   version '1.0.0'
-  url 'www.easyredmine.com'
+  url 'https://www.easysoftware.com'
 
-  settings  partial: 'settings/redmine_social_sign_in',
-            default: {
-              'google_enabled' => '0',
-              'google_app_id' => '',
-              'google_app_secret' => '',
-              'facebook_enabled' => '0',
-              'facebook_app_id' => '',
-              'facebook_app_secret' => '',
-              'linkedin_enabled' => '0',
-              'linkedin_app_id' => '',
-              'linkedin_app_secret' => '',
-              'twitter_enabled' => '0',
-              'twitter_app_id' => '',
-              'twitter_app_secret' => ''
-            }
+  settings partial: 'settings/redmine_social_sign_in',
+           default: {
+               'google_enabled'        => '0',
+               'google_app_id'         => '',
+               'google_app_secret'     => '',
+               'facebook_enabled'      => '0',
+               'facebook_app_id'       => '',
+               'facebook_app_secret'   => '',
+               'linkedin_enabled'      => '0',
+               'linkedin_app_id'       => '',
+               'linkedin_app_secret'   => '',
+               'twitter_enabled'       => '0',
+               'twitter_app_id'        => '',
+               'twitter_app_secret'    => '',
+               'qq_connect_enabled'    => '0',
+               'qq_connect_app_id'     => '',
+               'qq_connect_app_secret' => ''
+           }
 end
 
 if Redmine::Plugin.registered_plugins[:easy_extensions].nil?
